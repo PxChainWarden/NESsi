807667363629e583d5b3bf3b4bed1d67a1b48320
Alican Küsar <45844905+PxChainWarden@users.noreply.github.com>
2022-11-17 22:41:23 +0300

    Completed the lookup map for instructions. Added more Instruction definitions such as "ORA","TXS" etc.

--- px6502CPU.cpp
+++ px6502CPU.cpp
@@ -85,8 +85,143 @@ Px6502CPU::Px6502CPU(/* args */)
         {0xC0, {"CPY",&Px6502CPU::CPY,&Px6502CPU::IMM,2}},
         {0xC4, {"CPY",&Px6502CPU::CPY,&Px6502CPU::ZP0,3}},
         {0xCC, {"CPY",&Px6502CPU::CPY,&Px6502CPU::ABS,4}},
-
-
+        // Decrement Memory
+        {0xC6, {"DEC",&Px6502CPU::DEC,&Px6502CPU::ZP0,5}},
+        {0xD6, {"DEC",&Px6502CPU::DEC,&Px6502CPU::ZPX,6}},
+        {0xCE, {"DEC",&Px6502CPU::DEC,&Px6502CPU::ABS,6}},
+        {0xDE, {"DEC",&Px6502CPU::DEC,&Px6502CPU::ABSX,7}},
+        // Decrement X Register
+        {0xCA, {"DEX",&Px6502CPU::DEX,&Px6502CPU::IMP,2}},
+        // Decrement Y Register
+        {0x88, {"DEY",&Px6502CPU::DEY,&Px6502CPU::IMP,2}},
+        // Exclusive OR
+        {0x49, {"EOR",&Px6502CPU::EOR,&Px6502CPU::IMM,2}},
+        {0x45, {"EOR",&Px6502CPU::EOR,&Px6502CPU::ZP0,3}},
+        {0x55, {"EOR",&Px6502CPU::EOR,&Px6502CPU::ZPX,4}},
+        {0x4D, {"EOR",&Px6502CPU::EOR,&Px6502CPU::ABS,4}},
+        {0x5D, {"EOR",&Px6502CPU::EOR,&Px6502CPU::ABSX,4}},
+        {0x59, {"EOR",&Px6502CPU::EOR,&Px6502CPU::ABSY,4}},
+        {0x41, {"EOR",&Px6502CPU::EOR,&Px6502CPU::INDX,6}},
+        {0x51, {"EOR",&Px6502CPU::EOR,&Px6502CPU::INDY,5}},
+        // Increment Memory
+        {0xE6, {"INC",&Px6502CPU::INC,&Px6502CPU::ZP0,5}},
+        {0xF6, {"INC",&Px6502CPU::INC,&Px6502CPU::ZPX,6}},
+        {0xEE, {"INC",&Px6502CPU::INC,&Px6502CPU::ABS,6}},
+        {0xFE, {"INC",&Px6502CPU::INC,&Px6502CPU::ABSX,7}},
+        // Increment X Register
+        {0xE8, {"INX",&Px6502CPU::INX,&Px6502CPU::IMP,2}},
+        // Increment Y Register
+        {0xC8, {"INY",&Px6502CPU::INY,&Px6502CPU::IMP,2}},
+        // Jump
+        {0x4C, {"JMP",&Px6502CPU::JMP,&Px6502CPU::ABS,3}},
+        {0x6C, {"JMP",&Px6502CPU::JMP,&Px6502CPU::IND,5}},
+        // Jump Subroutine
+        {0x20, {"JSR",&Px6502CPU::JSR,&Px6502CPU::ABS,6}},
+        // Load Accumulator
+        {0xA9, {"LDA",&Px6502CPU::LDA,&Px6502CPU::IMM,2}},
+        {0xA5, {"LDA",&Px6502CPU::LDA,&Px6502CPU::ZP0,3}},
+        {0xB5, {"LDA",&Px6502CPU::LDA,&Px6502CPU::ZPX,4}},
+        {0xAD, {"LDA",&Px6502CPU::LDA,&Px6502CPU::ABS,4}},
+        {0xBD, {"LDA",&Px6502CPU::LDA,&Px6502CPU::ABSX,4}},
+        {0xB9, {"LDA",&Px6502CPU::LDA,&Px6502CPU::ABSY,4}},
+        {0xA1, {"LDA",&Px6502CPU::LDA,&Px6502CPU::INDX,6}},
+        {0xB1, {"LDA",&Px6502CPU::LDA,&Px6502CPU::INDY,5}},
+        // Load X Register
+        {0xA2, {"LDX",&Px6502CPU::LDX,&Px6502CPU::IMM,2}},
+        {0xA6, {"LDX",&Px6502CPU::LDX,&Px6502CPU::ZP0,3}},
+        {0xB6, {"LDX",&Px6502CPU::LDX,&Px6502CPU::ZPY,4}},
+        {0xAE, {"LDX",&Px6502CPU::LDX,&Px6502CPU::ABS,4}},
+        {0xBE, {"LDX",&Px6502CPU::LDX,&Px6502CPU::ABSY,4}},
+        // Load Y Register
+        {0xA2, {"LDY",&Px6502CPU::LDY,&Px6502CPU::IMM,2}},
+        {0xA6, {"LDY",&Px6502CPU::LDY,&Px6502CPU::ZP0,3}},
+        {0xB6, {"LDY",&Px6502CPU::LDY,&Px6502CPU::ZPX,4}},
+        {0xAE, {"LDY",&Px6502CPU::LDY,&Px6502CPU::ABS,4}},
+        {0xBE, {"LDY",&Px6502CPU::LDY,&Px6502CPU::ABSX,4}},
+        // Logical Shift Right
+        {0x4A, {"LSR",&Px6502CPU::LSR,&Px6502CPU::ACC,2}},
+        {0x46, {"LSR",&Px6502CPU::LSR,&Px6502CPU::ZP0,5}},
+        {0x56, {"LSR",&Px6502CPU::LSR,&Px6502CPU::ZPX,6}},
+        {0x4E, {"LSR",&Px6502CPU::LSR,&Px6502CPU::ABS,6}},
+        {0x5E, {"LSR",&Px6502CPU::LSR,&Px6502CPU::ABSX,7}},
+        // No Operation
+        {0xEA, {"NOP",&Px6502CPU::NOP,&Px6502CPU::IMP,2}},
+        // Logical Inclusive OR
+        {0x09, {"ORA",&Px6502CPU::ORA,&Px6502CPU::IMM,2}},
+        {0x05, {"ORA",&Px6502CPU::ORA,&Px6502CPU::ZP0,3}},
+        {0x15, {"ORA",&Px6502CPU::ORA,&Px6502CPU::ZPX,4}},
+        {0x0D, {"ORA",&Px6502CPU::ORA,&Px6502CPU::ABS,4}},
+        {0x1D, {"ORA",&Px6502CPU::ORA,&Px6502CPU::ABSX,4}},
+        {0x19, {"ORA",&Px6502CPU::ORA,&Px6502CPU::ABSY,4}},
+        {0x01, {"ORA",&Px6502CPU::ORA,&Px6502CPU::INDX,6}},
+        {0x11, {"ORA",&Px6502CPU::ORA,&Px6502CPU::INDY,5}},
+        // Push Accumulator
+        {0x48, {"PHA",&Px6502CPU::PHA,&Px6502CPU::IMP,3}},
+        // Push Processor Status
+        {0x08, {"PHP",&Px6502CPU::PHP,&Px6502CPU::IMP,3}},
+        // Pull Accumulator
+        {0x68, {"PLA",&Px6502CPU::PLA,&Px6502CPU::IMP,4}},
+        // Pull Processor Status
+        {0x28, {"PLP",&Px6502CPU::PLP,&Px6502CPU::IMP,4}},
+        // Rotate Left
+        {0x2A, {"ROL",&Px6502CPU::ROL,&Px6502CPU::ACC,2}},
+        {0x26, {"ROL",&Px6502CPU::ROL,&Px6502CPU::ZP0,5}},
+        {0x36, {"ROL",&Px6502CPU::ROL,&Px6502CPU::ZPX,6}},
+        {0x2E, {"ROL",&Px6502CPU::ROL,&Px6502CPU::ABS,6}},
+        {0x3E, {"ROL",&Px6502CPU::ROL,&Px6502CPU::ABSX,7}},
+        // Rotate Right
+        {0x6A, {"ROR",&Px6502CPU::ROR,&Px6502CPU::ACC,2}},
+        {0x66, {"ROR",&Px6502CPU::ROR,&Px6502CPU::ZP0,5}},
+        {0x76, {"ROR",&Px6502CPU::ROR,&Px6502CPU::ZPX,6}},
+        {0x6E, {"ROR",&Px6502CPU::ROR,&Px6502CPU::ABS,6}},
+        {0x7E, {"ROR",&Px6502CPU::ROR,&Px6502CPU::ABSX,7}},
+        // Return from Interrupt
+        {0x40, {"RTI",&Px6502CPU::RTI,&Px6502CPU::IMP,6}},
+        // Return from Subroutine
+        {0x60, {"RTS",&Px6502CPU::RTS,&Px6502CPU::IMP,6}},
+        // Substract with Carry
+        {0xE9, {"SBC",&Px6502CPU::SBC,&Px6502CPU::IMM,2}},
+        {0xE5, {"SBC",&Px6502CPU::SBC,&Px6502CPU::ZP0,3}},
+        {0xF5, {"SBC",&Px6502CPU::SBC,&Px6502CPU::ZPX,4}},
+        {0xED, {"SBC",&Px6502CPU::SBC,&Px6502CPU::ABS,4}},
+        {0xFD, {"SBC",&Px6502CPU::SBC,&Px6502CPU::ABSX,4}},
+        {0xF9, {"SBC",&Px6502CPU::SBC,&Px6502CPU::ABSY,4}},
+        {0xE1, {"SBC",&Px6502CPU::SBC,&Px6502CPU::INDX,6}},
+        {0xF1, {"SBC",&Px6502CPU::SBC,&Px6502CPU::INDY,5}},
+        // Set Carry Flag
+        {0x38, {"SEC",&Px6502CPU::SEC,&Px6502CPU::IMP,2}},
+        // Set Decimal Flag
+        {0xF8, {"SED",&Px6502CPU::SED,&Px6502CPU::IMP,2}},
+        // Set Interrupt Disable
+        {0x78, {"SEI",&Px6502CPU::SEI,&Px6502CPU::IMP,2}},
+        // Store Accumulator
+        {0x85, {"STA",&Px6502CPU::STA,&Px6502CPU::ZP0,3}},
+        {0x95, {"STA",&Px6502CPU::STA,&Px6502CPU::ZPX,4}},
+        {0x8D, {"STA",&Px6502CPU::STA,&Px6502CPU::ABS,4}},
+        {0x9D, {"STA",&Px6502CPU::STA,&Px6502CPU::ABSX,5}},
+        {0x99, {"STA",&Px6502CPU::STA,&Px6502CPU::ABSY,5}},
+        {0x81, {"STA",&Px6502CPU::STA,&Px6502CPU::INDX,6}},
+        {0x91, {"STA",&Px6502CPU::STA,&Px6502CPU::INDY,6}},
+        // Store X Register
+        {0x89, {"STX",&Px6502CPU::STX,&Px6502CPU::ZP0,3}},
+        {0x96, {"STX",&Px6502CPU::STX,&Px6502CPU::ZPY,4}},
+        {0x8E, {"STX",&Px6502CPU::STX,&Px6502CPU::ABS,4}},
+        // Store Y Register
+        {0x84, {"STY",&Px6502CPU::STY,&Px6502CPU::ZP0,3}},
+        {0x94, {"STY",&Px6502CPU::STY,&Px6502CPU::ZPX,4}},
+        {0x8C, {"STY",&Px6502CPU::STY,&Px6502CPU::ABS,4}},
+        // Transfer Accumulator to Register X
+        {0xAA, {"TAX",&Px6502CPU::TAX,&Px6502CPU::IMP,2}},
+        // Transfer Accumulator to Register Y
+        {0xA8, {"TAY",&Px6502CPU::TAY,&Px6502CPU::IMP,2}},
+        // Transfer Stack Pointer to Register X
+        {0xBA, {"TSX",&Px6502CPU::TSX,&Px6502CPU::IMP,2}},
+        // Transfer X Register to Accumulator
+        {0x8A, {"TXA",&Px6502CPU::TXA,&Px6502CPU::IMP,2}},
+        // Transfer X Register to Stack Pointer
+        {0x9A, {"TXS",&Px6502CPU::TXS,&Px6502CPU::IMP,2}},
+        // Transfer Y Register to Accumulator
+        {0x98, {"TYA",&Px6502CPU::TYA,&Px6502CPU::IMP,2}},
     };
 }
 
@@ -124,6 +259,7 @@ uint8_t Px6502CPU::getFlag(FLAGS flag){
     return 0;
 }
 
+// =========================================== ADDRESSING MODES ===========================================
 // Accumulator Addressing mode
 // The operand A is implicitly defined as the accumulator with a 1 byte instruction.
 uint8_t Px6502CPU::ACC(){
@@ -216,7 +352,7 @@ uint8_t Px6502CPU::ABSX(){
 
     effective_address = ( (operand_MSB << 8) + operand_LSB ) + x;
 
-    if( (effective_address & 0xFF00) != (operand_MSB << 8))
+    if( (effective_address & 0xFF00) != (operand_MSB << 8))             // return additional cycle count if memory page is changed
         return 1;
     return 0;
 }
@@ -237,7 +373,7 @@ uint8_t Px6502CPU::ABSY(){
 
     effective_address = ( (operand_MSB << 8) + operand_LSB ) + y;
 
-    if( (effective_address & 0xFF00) != (operand_MSB << 8))
+    if( (effective_address & 0xFF00) != (operand_MSB << 8))             // return additional cycle count if memory page is changed
         return 1;
     return 0;
 }
@@ -287,25 +423,30 @@ uint8_t Px6502CPU::INDY(){
     uint8_t MSB = read(operand + 0x01);
     
 
-    effective_address = (( MSB << 8) + LSB + y);      // Addition with carry because effective address is 16 bit and y is a 8 bit register.
+    effective_address = (( MSB << 8) + LSB + y);        // Addition with carry because effective address is 16 bit and y is a 8 bit register.
 
+    if( (effective_address & 0xFF00) != (MSB << 8) )    // return additional cycle count if memory page is changed
+        return 1;                                       
     return 0;
 }
 
 // A function for fetching data after effective address is set by the Addressing Mode
 void Px6502CPU::fetch(){
-    fethced_data = read(effective_address);
+    if( operationLookup[opcode].pAddressingFunction != &Px6502CPU::IMP )
+        fethced_data = read(effective_address);
 }
 
-// TODO
+// =========================================== INSTRUCTIONS ===========================================
+// Instruction Set for MOS 6502 CPU
+// More details can be found here: https://www.nesdev.org/obelisk-6502-guide/reference.html
+
 uint8_t Px6502CPU::ADC(){
 
     return 0;
 }
 
 uint8_t Px6502CPU::AND(){
-    fetch();
-    a = a & fethced_data;
+    a = a & read(effective_address);
     if(a == 0x00)
         setFlag(Z,true);
     if( (a & 0b10000000) > 0)
@@ -358,41 +499,298 @@ uint8_t Px6502CPU::BVS(){
 }
 
 uint8_t Px6502CPU::CLC(){
+    setFlag(C,false);
     return 0;
 }
 
 uint8_t Px6502CPU::CLD(){
+    setFlag(D,false);
     return 0;
 }
 
 uint8_t Px6502CPU::CLI(){
+    setFlag(I,false);
     return 0;
 }
 
 uint8_t Px6502CPU::CLV(){
+    setFlag(V,false);
     return 0;
 }
 
 uint8_t Px6502CPU::CMP(){
+    uint8_t compare = a - read(effective_address);
+    if(compare == 0x00)                   // Set Zero Flag if loaded data is 0
+        setFlag(Z,true);
+    if( (compare & 0b10000000) > 0)       // Set Negative Flag if bit 7 is set
+        setFlag(N,true);
+    if (a >= read(effective_address))     // Set Carry Flag if Accumulator is greater than Memory Data
+        setFlag(C,true);
+    return 0;
     return 0;
 }
 
 uint8_t Px6502CPU::CPX(){
+    uint8_t compare = x - read(effective_address);
+    if(compare == 0x00)                   // Set Zero Flag if loaded data is 0
+        setFlag(Z,true);
+    if( (compare & 0b10000000) > 0)       // Set Negative Flag if bit 7 is set
+        setFlag(N,true);
+    if (x >= read(effective_address))     // Set Carry Flag if X Register is greater than Memory Data
+        setFlag(C,true);
+    return 0;
     return 0;
 }
 
 uint8_t Px6502CPU::CPY(){
+    uint8_t compare = y - read(effective_address);
+    if(compare == 0x00)                   // Set Zero Flag if loaded data is 0
+        setFlag(Z,true);
+    if( (compare & 0b10000000) > 0)       // Set Negative Flag if bit 7 is set
+        setFlag(N,true);
+    if (y >= read(effective_address))     // Set Carry Flag if Y Register is greater than Memory Data
+        setFlag(C,true);
+    return 0;
+}
+
+uint8_t Px6502CPU::DEC(){
+    fethced_data = read(effective_address);
+    fethced_data--;
+    write(effective_address,fethced_data);
+    if(fethced_data == 0x00)                   // Set Zero Flag if loaded data is 0
+        setFlag(Z,true);
+    if( (fethced_data & 0b10000000) > 0)       // Set Negative Flag if bit 7 is set
+        setFlag(N,true);
+    return 0;
+}
+
+uint8_t Px6502CPU::DEX(){
+    x--;
+    if(x == 0x00)                   // Set Zero Flag if loaded data is 0
+        setFlag(Z,true);
+    if( (x & 0b10000000) > 0)       // Set Negative Flag if bit 7 is set
+        setFlag(N,true);
+    return 0;
+}
+
+uint8_t Px6502CPU::DEY(){
+    y--;
+    if(y == 0x00)                   // Set Zero Flag if loaded data is 0
+        setFlag(Z,true);
+    if( (y & 0b10000000) > 0)       // Set Negative Flag if bit 7 is set
+        setFlag(N,true);
+    return 0;
+}
+
+uint8_t Px6502CPU::EOR(){
+    a = a ^ read(effective_address);
+    if(a == 0x00)                   // Set Zero Flag if loaded data is 0
+        setFlag(Z,true);
+    if( (a & 0b10000000) > 0)       // Set Negative Flag if bit 7 is set
+        setFlag(N,true);
+    return 0;
+}
+
+uint8_t Px6502CPU::INC(){
+    fethced_data = read(effective_address);
+    fethced_data++;
+    write(effective_address,fethced_data);
+    if(fethced_data == 0x00)                   // Set Zero Flag if loaded data is 0
+        setFlag(Z,true);
+    if( (fethced_data & 0b10000000) > 0)       // Set Negative Flag if bit 7 is set
+        setFlag(N,true);
+    return 0;
+}
+
+uint8_t Px6502CPU::INX(){
+    x++;
+    if(x == 0x00)                   // Set Zero Flag if loaded data is 0
+        setFlag(Z,true);
+    if( (x & 0b10000000) > 0)       // Set Negative Flag if bit 7 is set
+        setFlag(N,true);
+    return 0;
+}
+
+uint8_t Px6502CPU::INY(){
+    y++;
+    if(y == 0x00)                   // Set Zero Flag if loaded data is 0
+        setFlag(Z,true);
+    if( (y & 0b10000000) > 0)       // Set Negative Flag if bit 7 is set
+        setFlag(N,true);
+    return 0;
+}
+
+uint8_t Px6502CPU::JMP(){
+    return 0;
+}
+
+uint8_t Px6502CPU::JSR(){
     return 0;
 }
 
 uint8_t Px6502CPU::LDA(){
-    fetch();
-    a = fethced_data;
+    a = read(effective_address);
 
     if(a == 0x00)                   // Set Zero Flag if loaded data is 0
         setFlag(Z,true);
     if( (a & 0b10000000) > 0)       // Set Negative Flag if bit 7 is set
         setFlag(N,true);
 
+    return 0;
+}
+
+uint8_t Px6502CPU::LDX(){
+    x = read(effective_address);
+
+    if(x == 0x00)                   // Set Zero Flag if loaded data is 0
+        setFlag(Z,true);
+    if( (x & 0b10000000) > 0)       // Set Negative Flag if bit 7 is set
+        setFlag(N,true);
+
+    return 0;
+}
+
+uint8_t Px6502CPU::LDY(){
+    y = read(effective_address);
+
+    if(y == 0x00)                   // Set Zero Flag if loaded data is 0
+        setFlag(Z,true);
+    if( (y & 0b10000000) > 0)       // Set Negative Flag if bit 7 is set
+        setFlag(N,true);
+
+    return 0;
+}
+
+uint8_t Px6502CPU::LSR(){
+    
+    return 0;
+}
+
+uint8_t Px6502CPU::NOP(){
+    return 0;
+}
+
+uint8_t Px6502CPU::ORA(){
+    a = a | read(effective_address);
+    if(a == 0x00)                   // Set Zero Flag if loaded data is 0
+        setFlag(Z,true);
+    if( (a & 0b10000000) > 0)       // Set Negative Flag if bit 7 is set
+        setFlag(N,true);
+    return 0;
+}
+
+uint8_t Px6502CPU::PHA(){
+    return 0;
+}
+
+uint8_t Px6502CPU::PHP(){
+    return 0;
+}
+
+uint8_t Px6502CPU::PLA(){
+    return 0;
+}
+
+uint8_t Px6502CPU::PLP(){
+    return 0;
+}
+
+uint8_t Px6502CPU::ROL(){
+    return 0;
+}
+
+uint8_t Px6502CPU::ROR(){
+    return 0;
+}
+
+uint8_t Px6502CPU::RTI(){
+    return 0;
+}
+
+uint8_t Px6502CPU::RTS(){
+    return 0;
+}
+
+uint8_t Px6502CPU::SBC(){
+    return 0;
+}
+
+uint8_t Px6502CPU::SEC(){
+    setFlag(C,true);
+    return 0;
+}
+
+uint8_t Px6502CPU::SED(){
+    setFlag(D,true);
+    return 0;
+}
+
+uint8_t Px6502CPU::SEI(){
+    setFlag(I,true);
+    return 0;
+}
+
+uint8_t Px6502CPU::STA(){
+    write(effective_address,a);
+    return 0;
+}
+
+uint8_t Px6502CPU::STX(){
+    write(effective_address,x);
+    return 0;
+}
+
+uint8_t Px6502CPU::STY(){
+    write(effective_address,y);
+    return 0;
+}
+
+uint8_t Px6502CPU::TAX(){
+    x = a;
+    if(x == 0x00)                   // Set Zero Flag if loaded data is 0
+        setFlag(Z,true);
+    if( (x & 0b10000000) > 0)       // Set Negative Flag if bit 7 is set
+        setFlag(N,true);
+    return 0;
+}
+
+uint8_t Px6502CPU::TAY(){
+    y = a;
+    if(y == 0x00)                   // Set Zero Flag if loaded data is 0
+        setFlag(Z,true);
+    if( (y & 0b10000000) > 0)       // Set Negative Flag if bit 7 is set
+        setFlag(N,true);
+    return 0;
+}
+
+uint8_t Px6502CPU::TSX(){
+    x = sp;
+    if(x == 0x00)                   // Set Zero Flag if loaded data is 0
+        setFlag(Z,true);
+    if( (x & 0b10000000) > 0)       // Set Negative Flag if bit 7 is set
+        setFlag(N,true);
+    return 0;
+}
+
+uint8_t Px6502CPU::TXA(){
+    a = x;
+    if(a == 0x00)                   // Set Zero Flag if loaded data is 0
+        setFlag(Z,true);
+    if( (a & 0b10000000) > 0)       // Set Negative Flag if bit 7 is set
+        setFlag(N,true);
+    return 0;
+}
+
+uint8_t Px6502CPU::TXS(){
+    sp = x;
+    return 0;
+}
+
+uint8_t Px6502CPU::TYA(){
+    a = y;
+    if(a == 0x00)                   // Set Zero Flag if loaded data is 0
+        setFlag(Z,true);
+    if( (a & 0b10000000) > 0)       // Set Negative Flag if bit 7 is set
+        setFlag(N,true);
     return 0;
 }
